5ede2db84c6e0fefe1b1d8c089a639fa3be74218
pawifinimangzimo <pwifimzimo@gmail.com>
2025-05-13 11:44:56 +0800

    Update validator.py

--- core/validator.py
+++ core/validator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from collections import defaultdict
+import yaml  # Added this import
 
 class LotteryValidator:
     def __init__(self, config_path="config.yaml"):
